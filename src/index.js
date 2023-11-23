60e1c14bc16973c28f2a7d40cfd1129d049766c6
Gaby Nieva <gaby.nieva@outlook.com>
2023-11-22 23:21:31 -0600

    Agregando estilo y usando la API de internacionalización

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@
 const baseUrl = "https://platzi-avo.vercel.app";
 const appNode = document.querySelector('#app');
 
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-En', {
+        style: 'currency',
+        currency: "USD",
+    }).format(price)
+
+    return newPrice;
+}
+
+
 // Web API
 // Conectarnos al server
 window
@@ -27,19 +37,42 @@ window
             // crear título
             const title = document.createElement("h2");
             title.textContent = item.name;
+            //title.style = 'font-size: 2rem' Así se puede usar también
+            //title.style.fontSize = "3rem"; Manipulando la propiedad fontSize
+            // title.className = "muy-grande"; Así se pueden usar clases css
+            // text-xl es una clase del framework tailwind
+            title.className = "text-2xl text-red-600"; 
             //document.body.appendChild(title);
 
             // crear precio
             const price = document.createElement("div");
-            price.textContent = item.price;
+            price.className = "text-gray-600";
+            price.textContent = formatPrice(item.price);
             //document.body.appendChild(price);
 
-            const container = document.createElement('div');
-            container.append(imagen, title, price);
-            todosLosItems.push(container);
+            // const container = document.createElement('div');
+            // container.append(imagen, title, price); todosLosItems.push(container);
             // container.appendChild(image);
             // container.appendChild(title);
             // container.appendChild(price);
+         // Creamos un contenedor el título y el precio
+         
+         const priceAndTitle = document.createElement("div")
+         priceAndTitle.className = "text-center md:text-left";
+         priceAndTitle.appendChild(title);
+         priceAndTitle.appendChild(price);
+
+         // Metemos todo dentro de una tarjeta contenedora
+         const card = document.createElement("div");
+         card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+         card.append(imagen, priceAndTitle);
+
+        // Metemos todo dentro del contenedor principal
+        const contenedor = document.createElement("div");
+        contenedor.appendChild(card);
+         
+        todosLosItems.push(contenedor);
+        
         });
 
        appNode.append(...todosLosItems);
